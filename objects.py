b79a0649a520eae03a585acced5705d71cd0078b
Faith-yiamat <oloserianyiamat@gmail.com>
2024-05-24 20:08:09 +0300

    push changes

---
+++ objects.py
@@ -0,0 +1,4 @@
+# if you want to create an objectin python we create a class that represent the type of object we want to create
+# A class contains attributes (variables), methods(functions) 
+# a class can contain an optional constructor
+
